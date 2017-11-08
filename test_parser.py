2ca2dcd171283397be8c218fbabb9e7ecd6fd806
Masahiro Sakai <sakai@preferred.jp>
2017-11-08 15:09:26 +0900

    add a script to test parser.py

---
+++ test_parser.py
@@ -0,0 +1,22 @@
+import itertools
+import sys
+import parsy
+import holstep
+import parser
+
+sys.setrecursionlimit(10000)
+
+num_error = 0
+
+for fname in itertools.chain(("holstep/train/%05d" % i for i in range(1,10000)), ("holstep/test/%04d" % i for i in range(1,1412))):
+    print(fname)
+    file = holstep.read_file(fname)
+    for formula in itertools.chain([file.conjecture], file.dependencies, file.examples):
+        try:
+            parser.judgement.parse(formula.text)
+        except parsy.ParseError as ex:
+            print(ex)
+            print(formula.text)
+            num_error += 1
+            if num_error >= 10:
+                exit(1)
