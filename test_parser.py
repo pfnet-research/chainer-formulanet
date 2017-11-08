29c778a60c6bdffb8eeaf52ee352a99b88909d40
Masahiro Sakai <sakai@preferred.jp>
2017-11-08 16:06:12 +0900

    change test_parser.py to process next file when if it encounter 10 errors

--- test_parser.py
+++ test_parser.py
@@ -6,11 +6,10 @@ import parser
 
 sys.setrecursionlimit(10000)
 
-num_error = 0
-
 for fname in itertools.chain(("holstep/train/%05d" % i for i in range(1,10000)), ("holstep/test/%04d" % i for i in range(1,1412))):
     print(fname)
     file = holstep.read_file(fname)
+    num_error = 0
     for formula in itertools.chain([file.conjecture], file.dependencies, file.examples):
         try:
             parser.judgement.parse(formula.text)
@@ -19,4 +18,4 @@ for fname in itertools.chain(("holstep/train/%05d" % i for i in range(1,10000)),
             print(formula.text)
             num_error += 1
             if num_error >= 10:
-                exit(1)
+                break
