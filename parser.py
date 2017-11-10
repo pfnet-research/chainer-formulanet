b9ec8b220f44e278b1c7c91682688ea0e75280b8
Masahiro Sakai <sakai@preferred.jp>
2017-11-10 16:10:58 +0900

    separate expression types from parser.py into expr.py

--- parser.py
+++ parser.py
@@ -1,42 +1,7 @@
 import parsy
 from parsy import alt, fail, generate, regex, seq, string
-
-class Expr(object):
-    pass
-
-class EIdent(Expr):
-    def __init__(self, name):
-        self.name = name
-
-    def __str__(self):
-        return self.name
-
-class EApply(Expr):
-    def __init__(self, fun, arg):
-        self.fun = fun
-        self.arg = arg
-
-    # 注意: 中置演算子なども前置の正しくない形式で文字列化される
-    def __str__(self):
-        return "({} {})".format(str(self.fun), str(self.arg))
-
-class EQuantified(Expr):
-    def __init__(self, binder, var, body):
-        self.binder = binder
-        self.var = var
-        self.body = body
-
-    def __str__(self):
-        return "({} {}. {})".format(self.binder, self.var, str(self.body))
-
-class Thm:
-    def __init__(self, premises, conclusion):
-        self.premises = tuple(premises)
-        self.conclusion = conclusion
-
-    def __str__(self):
-        return "{} |- {}".format(",".join(map(str, self.premises)), str(self.conclusion))
-
+import expr as e
+from expr import EIdent, EQuantified, EApply, Thm
 
 spaces = regex(r'\s*')
 
@@ -51,8 +16,6 @@ rparen = lexme(string(')'))
 
 dot = lexme(string("."))
 
-bin_ops = frozenset(["<=>", "==>", "\\/", "/\\", "==", "===", "treal_eq", "IN", "<", "<<", "<<<", "<<=", "<=", "<=_c", "<_c", "=", "=_c", ">", ">=", ">=_c", ">_c", "HAS_SIZE", "PSUBSET", "SUBSET", "divides", "has_inf", "has_sup", "treal_le", ",", "..", "+", "++", "UNION", "treal_add", "-", "DIFF", "*", "**", "INTER", "INTERSECTION_OF", "UNION_OF", "treal_mul", "INSERT", "DELETE", "CROSS", "PCROSS", "/", "DIV", "MOD", "div", "rem", "EXP", "pow", "$", "o", "%", "%%", "-->", "--->"])
-
 @generate
 def expr_cont_quantified():
     b = yield lexme(regex(r"@|!|\?!|\?|\\|lambda"))
@@ -65,7 +28,7 @@ def expr_cont_quantified():
 @generate
 def binop():
     op = yield ident
-    if op not in bin_ops:
+    if op not in e.bin_ops:
         yield fail("{} is not a binary operator".format(op))
     else:
         return op
